8367066f04bb9703da5605d615bcdb6c87cac17d
Osayame Gaius-Obaseki <osayame@gmail.com>
2017-07-09 22:02:18 -0400

    add after_join and new_msg handlers

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,10 +1,25 @@
 defmodule ChatServer.RoomChannel do
   use Phoenix.Channel
 
+  @initial_posts ["post1", "post2"]
+
   def join("room:lobby", _message, socket) do
+    send self(), :after_join
     {:ok, socket}
   end
   def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
+  end
+
+  def handle_info(:after_join, socket) do
+    push socket, "new_posts", %{value: @initial_posts}
+
+    {:noreply, socket}
+  end
+
+  def handle_in("new:msg", msg, socket) do
+    push socket, "new_posts", %{value: @initial_posts ++ msg}
+
+    {:noreply, socket}
   end
 end
